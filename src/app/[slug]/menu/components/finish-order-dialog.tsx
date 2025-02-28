20bf1034edf00f334a691c13c2e64128885be2bf
leonbborges <leo.borges99@hotmail.com>
2025-02-27 23:19:22 -0300

    feat: ensure order can be created

--- src/app/[slug]/menu/components/finish-order-dialog.tsx
+++ src/app/[slug]/menu/components/finish-order-dialog.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import type { ConsumptionMethod } from "@prisma/client";
+import { Loader2Icon } from "lucide-react";
+import { useParams, useSearchParams } from "next/navigation";
+import { useContext, useTransition } from "react";
 import { useForm } from "react-hook-form";
 import { PatternFormat } from "react-number-format";
+import { toast } from "sonner";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -26,107 +31,133 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
-import { isValidCpf } from "../helpersMenu/cpf";
+import { createOrder } from "../actions/create-order";
+import { CartContext } from "../contexts/cart";
+import { isValidCpf } from "../helpers/cpf";
 
 const formSchema = z.object({
-  name: z.string().trim().min(1, {
-    message: "O nome é obrigatório.",
-  }),
-  cpf: z
-    .string()
-    .trim()
-    .min(1, {
-      message: "O CPF é obrigatório.",
-    })
-    .refine((value) => isValidCpf(value), {
-      message: "CPF inválido.",
-    }),
+	name: z.string().trim().min(1, {
+		message: "O nome é obrigatório.",
+	}),
+	cpf: z
+		.string()
+		.trim()
+		.min(1, {
+			message: "O CPF é obrigatório.",
+		})
+		.refine((value) => isValidCpf(value), {
+			message: "CPF inválido.",
+		}),
 });
 
 type FormSchema = z.infer<typeof formSchema>;
 
 interface FinishOrderDialogProps {
-  open: boolean;
-  onOpenChange: (open: boolean) => void;
+	open: boolean;
+	onOpenChange: (open: boolean) => void;
 }
 
 const FinishOrderDialog = ({ open, onOpenChange }: FinishOrderDialogProps) => {
-  const form = useForm<FormSchema>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: "",
-      cpf: "",
-    },
-    shouldUnregister: true,
-  });
-  const onSubmit = (data: FormSchema) => {
-    console.log({ data });
-  };
-  return (
-    <Drawer open={open} onOpenChange={onOpenChange}>
-      <DrawerTrigger asChild></DrawerTrigger>
-      <DrawerContent>
-        <DrawerHeader>
-          <DrawerTitle>Finalizar Pedido</DrawerTitle>
-          <DrawerDescription>
-            Insira suas informações abaixo para finalizar o seu pedido.
-          </DrawerDescription>
-        </DrawerHeader>
-        <div className="p-5">
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Seu nome</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Digite seu nome..." {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="cpf"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Seu CPF</FormLabel>
-                    <FormControl>
-                      <PatternFormat
-                        placeholder="Digite seu CPF..."
-                        format="###.###.###-##"
-                        customInput={Input}
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+	const { slug } = useParams<{ slug: string }>();
+	const { products } = useContext(CartContext);
+	const searchParams = useSearchParams();
+	const [isPending, startTransition] = useTransition();
+	const form = useForm<FormSchema>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			name: "",
+			cpf: "",
+		},
+		shouldUnregister: true,
+	});
+	const onSubmit = async (data: FormSchema) => {
+		try {
+			const consumptionMethod = searchParams.get(
+				"consumptionMethod",
+			) as ConsumptionMethod;
+			startTransition(async () => {
+				await createOrder({
+					consumptionMethod,
+					customerCpf: data.cpf,
+					customerName: data.name,
+					products,
+					slug,
+				});
+				onOpenChange(false);
+				toast.success("Pedido finalizado com sucesso!");
+			});
+		} catch (error) {
+			console.error(error);
+		}
+	};
+	return (
+		<Drawer open={open} onOpenChange={onOpenChange}>
+			{/* biome-ignore lint/style/useSelfClosingElements: <explanation> */}
+<DrawerTrigger asChild></DrawerTrigger>
+			<DrawerContent>
+				<DrawerHeader>
+					<DrawerTitle>Finalizar Pedido</DrawerTitle>
+					<DrawerDescription>
+						Insira suas informações abaixo para finalizar o seu pedido.
+					</DrawerDescription>
+				</DrawerHeader>
+				<div className="p-5">
+					<Form {...form}>
+						<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+							<FormField
+								control={form.control}
+								name="name"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Seu nome</FormLabel>
+										<FormControl>
+											<Input placeholder="Digite seu nome..." {...field} />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="cpf"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Seu CPF</FormLabel>
+										<FormControl>
+											<PatternFormat
+												placeholder="Digite seu CPF..."
+												format="###.###.###-##"
+												customInput={Input}
+												{...field}
+											/>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
 
-              <DrawerFooter>
-                <Button
-                  type="submit"
-                  variant="destructive"
-                  className="rounded-full"
-                >
-                  Finalizar
-                </Button>
-                <DrawerClose asChild>
-                  <Button className="w-full rounded-full" variant="outline">
-                    Cancelar
-                  </Button>
-                </DrawerClose>
-              </DrawerFooter>
-            </form>
-          </Form>
-        </div>
-      </DrawerContent>
-    </Drawer>
-  );
+							<DrawerFooter>
+								<Button
+									type="submit"
+									variant="destructive"
+									className="rounded-full"
+									disabled={isPending}
+								>
+									{isPending && <Loader2Icon className="animate-spin" />}
+									Finalizar
+								</Button>
+								<DrawerClose asChild>
+									<Button className="w-full rounded-full" variant="outline">
+										Cancelar
+									</Button>
+								</DrawerClose>
+							</DrawerFooter>
+						</form>
+					</Form>
+				</div>
+			</DrawerContent>
+		</Drawer>
+	);
 };
 
 export default FinishOrderDialog;
